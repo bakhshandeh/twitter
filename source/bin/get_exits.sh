94791c654d2e15785c941540cb19d4fb0d70ac24
Reza <sadegh_152@yahoo.com>
2012-01-12 00:05:51 +0330

    commit

--- source/bin/get_exits.sh
+++ source/bin/get_exits.sh
@@ -1,2 +1,2 @@
-OUT=`echo -e "authenticate \"\"\ngetinfo circuit-status\nquit" | nc 127.0.0.1 9051 | grep PURPOSE | cut -d ' ' -f 3`
+OUT=`echo -e "authenticate \"\"\ngetinfo circuit-status\nquit" | nc 127.0.0.1 9051 | grep PURPOSE | cut -d ' ' -f 3| sed -e "s/[= ]//g"`
 echo $OUT
