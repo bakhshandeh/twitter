30f305de62a019b861a926bb1f3f8bed8e21f851
Reza <sadegh_152@yahoo.com>
2012-01-14 01:58:33 +0330

    commit

--- source/src/limit_count.py
+++ source/src/limit_count.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             auth.set_access_token("174566652-MOGbxytlmUHIN5tEMgl5rgqWdWaIQXYZ6XPyYKl1", "yem38OfoUbsoPZvOVr3k0n3X7JSUDYD8oxAKXvrJw6k")
             twitterApi = API(auth_handler=auth,
                     host='api.twitter.com', search_host='search.twitter.com',
-                     cache=FileCache("cache", timeout = -1), secure=False, api_root='/1', search_root='',
+                     cache=None, secure=False, api_root='/1', search_root='',
                     retry_count=0, retry_delay=0, retry_errors=None,
                     parser=None)
 	    
