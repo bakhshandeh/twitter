5c6e831f964a88479447814283b09a4cc966ae7e
root <root@reza-laptop.(none)>
2012-02-17 13:36:57 +0330

    commit

--- source/lib/tweeapi/manager.py
+++ source/lib/tweeapi/manager.py
@@ -5,7 +5,7 @@ import psycopg2.extras
 def followUsers(userInfo,  users):
     api = APISingleton.getInstanceForUser(userInfo)
     
-    cur = DBSingleton.getInstance().cursor()
+    cur = DBSingleton.getInstance().cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute("select nextval('tracking_seq')")
     id = cur.fetchone()[0]
     
@@ -16,6 +16,6 @@ def followUsers(userInfo,  users):
         api.create_friendship(user_id=user.id)
         cur.execute("""insert into tracking_users(tracking_id, current_id, user_id, user_screen_name)
                     values(%s,%s,%s,%s)""", (id, userId, user.id, user.screen_name))
-    cur.commit()
+    db.commit()
         
     
