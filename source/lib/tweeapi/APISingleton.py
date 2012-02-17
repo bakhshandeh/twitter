fc78ccef2b99983cce3e0b96eb81b345a8602a0e
Reza <sadegh_152@yahoo.com>
2012-02-17 12:23:38 +0330

    commit

--- source/lib/tweeapi/APISingleton.py
+++ source/lib/tweeapi/APISingleton.py
@@ -9,8 +9,10 @@ twitter_api = None
 def getInstance():
     global twitter_api
     if not twitter_api:
-        auth = tweepy.OAuthHandler("xg2hLKvf1nxw1TUALvx5xA", "MkX0lDUik0mJuc6nxserddbQDWd7ZTErQN6Tf0OhOM")
-        auth.set_access_token("174566652-MOGbxytlmUHIN5tEMgl5rgqWdWaIQXYZ6XPyYKl1", "yem38OfoUbsoPZvOVr3k0n3X7JSUDYD8oxAKXvrJw6k")
+        #auth = tweepy.OAuthHandler("xg2hLKvf1nxw1TUALvx5xA", "MkX0lDUik0mJuc6nxserddbQDWd7ZTErQN6Tf0OhOM")
+        auth = tweepy.OAuthHandler("hbSLc8Dwmdr3y1rUboyjw", "N76BUKBiYVtM5h5ddlaO4B2W9XNcjAV0uC80S47Ew")
+        auth.set_access_token("174566652-hByI99rkwfFcVP6er2SYRM86t2JDJxlrZ2a5MTGG", "23JGpYi4vaviI1q1hRs8xXVeaXvc7z4baycnB73SNyE")
+        #auth.set_access_token("174566652-MOGbxytlmUHIN5tEMgl5rgqWdWaIQXYZ6XPyYKl1", "yem38OfoUbsoPZvOVr3k0n3X7JSUDYD8oxAKXvrJw6k")
         twitter_api = API(auth_handler=auth,
                     host='api.twitter.com', search_host='search.twitter.com',
                     cache=DBFileCache(DBCache(timeout=-1, conn=DBSingleton.getInstance()), FileCache("cache", timeout=-1), timeout = -1), 
