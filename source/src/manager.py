848af2f6ca364ac23a43b78c0c8db390e67798a9
Reza <sadegh_152@yahoo.com>
2012-02-28 14:05:25 +0330

    commit

---
+++ source/src/manager.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("../lib/")
+from db import DBSingleton
+import psycopg2.extras
+
+from tweeapi import APISingleton, manager
+
+if __name__ == "__main__":
+
+    twitterApi = APISingleton.getInstance()
+
+    users = [twitterApi.get_user(sys.argv[1])]
+    db = DBSingleton.getInstance()
+    cur = DBSingleton.getInstance().cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    cur.execute("select * from site_users where manage=1")
+    row = cur.fetchone()
+    while row is not None:
+        print row
+    
+    #manager.followUsers(row, users)
