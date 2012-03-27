d2d3d2b63b2ae6acdd4db0620820ca9fab75842a
Reza <sadegh_152@yahoo.com>
2012-03-28 01:58:51 +0430

    commit

--- source/lib/tweeapi/APISingleton.py
+++ source/lib/tweeapi/APISingleton.py
@@ -10,8 +10,8 @@ def getInstance():
     global twitter_api
     if not twitter_api:
         #auth = tweepy.OAuthHandler("xg2hLKvf1nxw1TUALvx5xA", "MkX0lDUik0mJuc6nxserddbQDWd7ZTErQN6Tf0OhOM")
-        auth = tweepy.OAuthHandler("hbSLc8Dwmdr3y1rUboyjw", "N76BUKBiYVtM5h5ddlaO4B2W9XNcjAV0uC80S47Ew")
-        auth.set_access_token("174566652-hByI99rkwfFcVP6er2SYRM86t2JDJxlrZ2a5MTGG", "23JGpYi4vaviI1q1hRs8xXVeaXvc7z4baycnB73SNyE")
+        auth = tweepy.OAuthHandler("m32EakXMmUaGJW4rm8wXEQ", "TL5LSHWBNaUcW5MBG44V6piaF8JY9JPykMagtvfc")
+        auth.set_access_token("538432816-JPzoNG6xFMZ97tYthCZNKb9llfpL6rGOdNqHtZmD", "DtqSXyezDgio8JNYvHFmlML22vsonOEWCCWhZv7I")
         #auth.set_access_token("174566652-MOGbxytlmUHIN5tEMgl5rgqWdWaIQXYZ6XPyYKl1", "yem38OfoUbsoPZvOVr3k0n3X7JSUDYD8oxAKXvrJw6k")
         twitter_api = API(auth_handler=auth,
                     host='api.twitter.com', search_host='search.twitter.com',
@@ -26,7 +26,7 @@ def getInstanceForUser(userInfo):
     global twitterUsersApi
     userId = userInfo["oauth_uid"]
     if not twitterUsersApi.has_key(userId):
-        auth = tweepy.OAuthHandler("hbSLc8Dwmdr3y1rUboyjw", "N76BUKBiYVtM5h5ddlaO4B2W9XNcjAV0uC80S47Ew")
+        auth = tweepy.OAuthHandler("VtJIiRqZJ72wy4aXexXZvQ", "xA79MhAkTJdzy91hnULwfKeF54CfghZCkNWwxKdhrMc")
         auth.set_access_token(userInfo["oauth_token"], userInfo["oauth_secret"])
         twitterUsersApi[userId] = API(auth_handler=auth,
                     host='api.twitter.com', search_host='search.twitter.com',
